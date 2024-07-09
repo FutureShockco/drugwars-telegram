60be5cdd95a06562df2af16a320c5c9f64fe01b4
hightouch67 <cetinkaya.ozden@gmail.com>
2024-07-09 13:57:37 +0200

    up

--- src/components/Checkout.vue
+++ src/components/Checkout.vue
@@ -208,7 +208,7 @@ export default {
       const transaction = {
         messages: [
           {
-            address: "0:412410771DA82CBA306A55FA9E0D43C9D245E38133CB58F1457DFB8D5CD8892F", // destination address
+            address: "0:e5cbb3e6b91c6961fb7a75428e73a2e99f949b0ca4e49ceb066febe87704dcab", // destination address
             amount: "20000000" //Toncoin in nanotons
           }
         ]
